d84d7dbf0cf1544e05d2f65efcec64de1dd13f24
Lukasz Antkowiak <lantkowiak@infusion.com>
2016-02-28 23:37:15 +0100

    initial commit

---
+++ src/main/scala/pl/lantkowiak/sdm/MainActivity.scala
@@ -0,0 +1,42 @@
+package pl.lantkowiak.sdm
+
+import android.os.Bundle
+import android.support.design.widget.{Snackbar, FloatingActionButton}
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View.OnClickListener
+import android.view.{View, Menu, MenuItem}
+import pl.lantkowiak.simple_document_manager.R
+
+class MainActivity extends AppCompatActivity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val toolbar = findViewById(R.id.toolbar).asInstanceOf[Toolbar]
+    setSupportActionBar(toolbar)
+
+    val fab = findViewById(R.id.fab).asInstanceOf[FloatingActionButton]
+    val clickListener = new OnClickListener {
+      override def onClick(view: View): Unit = {
+        Snackbar.make(view, "Replace with your own action.", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+      }
+    }
+    fab.setOnClickListener(clickListener)
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    getMenuInflater().inflate(R.menu.menu_main, menu)
+
+    true
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    val id = item.getItemId()
+
+    if (id == R.id.action_settings) {
+      return true
+    }
+
+    super.onOptionsItemSelected(item)
+  }
+}
